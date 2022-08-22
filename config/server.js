b38a9bb53cebe0f02404c5fa151c1da90c44618c
Richard W. L. Brehmer <wlb.richard@gmail.com>
2022-08-22 08:04:04 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '820e0bd92e68ee14a06207119ab89525'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a541ce7f-9fac-4361-8940-f1ecabae23eb'
+};
