020b33110ebbb5bbfb573b337aaf8e2cb2e03a85
Richard W. L. Brehmer <wlb.richard@gmail.com>
2022-08-24 09:25:48 -0300

    Add custom service and controller to get sample games data

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,16 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (ctx) => {
+    console.log("Initializing...");
+
+    await strapi.services.game.populate();
+
+    ctx.send({ ok: true });
+  },
+};
--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,8 +1,40 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+const axios = require("axios");
+
+const getGameInfo = async (slug) => {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+
+  const formatedSlug = slug.replace(/-/g, "_");
+
+  const body = await axios.get(`https://www.gog.com/en/game/${formatedSlug}`);
+
+  const dom = new JSDOM(body.data);
+  const description = dom.window.document.querySelector(".description");
+
+  return {
+    rating: "BR0",
+    short_description: description?.textContent.slice(0, 166),
+    description: description.innerHTML,
+  };
+};
+
+module.exports = {
+  populate: async (params) => {
+    const gogApiUrl =
+      "https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Abestselling&productType=in%3Agame%2Cpack%2Cdlc%2Cextras&page=1&countryCode=BR&locale=en-US&currencyCode=BRL";
+
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    // console.log("products", products[0]);
+    console.log("getGameInfo", await getGameInfo(products[1].slug));
+  },
+};
